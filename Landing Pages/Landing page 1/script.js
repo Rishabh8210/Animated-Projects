ae20ce9f24a2983e5465f407a444bc44bb7abdaa
Rishabh Pandey <92287287+Rishabh8210@users.noreply.github.com>
2023-07-27 22:13:41 +0530

    Update script.js
    Created a function which is basically getting the rotational value of an element according to the responsiveness

--- Landing Pages/Landing page 1/script.js
+++ Landing Pages/Landing page 1/script.js
@@ -17,10 +17,55 @@ gsap.to(semicircleSpan[active-1],{
     opacity:1.3
 })
 
+
+// For getting Degree of transform
+
+function getDeg(element)
+{
+    const computedStyle = window.getComputedStyle(element);
+    const transformValue = computedStyle.getPropertyValue('transform');
+    const matrixPattern = /matrix\((.+?)\)/;
+    const match = transformValue.match(matrixPattern);
+    if (match) {
+      const matrixValues = match[1].split(',').map(parseFloat);
+      const a = matrixValues[0];
+      const b = matrixValues[1];
+      const rotationRad = Math.atan2(b, a);
+      const rotationDeg = (rotationRad * 180) / Math.PI;
+      return rotationDeg; // Output: 45
+    }
+    // else {
+    //   console.log("No rotation found.");
+    // }   
+}
+
+
+
+
+
+
+
+
+var element = document.querySelector('.str1');
+
+// Extracting the rotation value from the transform matrix
+
+const getDeg1 = Math.round(getDeg(element));
+// console.log(getDeg1)
+element = document.querySelector('.str2');
+const getDeg2 = Math.round(getDeg(element));
+// console.log(getDeg2)
+
+const diff = ((getDeg1*-1) - (getDeg2*-1))
+// console.log(diff)
+
+
+
+
 minicircles.forEach(function(miniC, index){
     miniC.addEventListener("click",function(){
         gsap.to(".circle", {
-            rotate: (3 - (index + 1))*10,
+            rotate: (3 - (index + 1))*diff,
             ease:Expo.easeInOut,
             duration:1
         })
